7dcfad2e6bf6ce759f4149e25013bd9ca9b7e17b
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2024-05-27 10:50:00 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,12 +4,12 @@ import os
 
 
 # your bot token
-B_TOKEN = "5229400507:AAFB8T-9nmVBrjVCEmoX9Ul-0NFgtmL4XvA"
+B_TOKEN = "6999255941:AAEwki0F3RoeJD14XkNKdRSt1GP-6ovHe70"
 #your api id
 API = 10248430
 # your api hash
 API_HASH = "42396a6ff14a569b9d59931643897d0d"
 # your bot username without @ like this ultron2_robot
-BOT_USERNAME = "ultron2_robot"
+BOT_USERNAME = "AriseApproveBot"
 # your owner id
-ownerid = 5470956337
+ownerid = 6720082402
