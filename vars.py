189531e592e1181e427677e829b1c42154014c2c
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2024-05-27 10:21:47 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -9,10 +9,6 @@ B_TOKEN = "5229400507:AAFB8T-9nmVBrjVCEmoX9Ul-0NFgtmL4XvA"
 API = 10248430
 # your api hash
 API_HASH = "42396a6ff14a569b9d59931643897d0d"
-# your group link
-SUPPORT = "https://t.me/thanosprosss"
-# your channel link
-UPDATE = "https://t.me/THANOS_PRO"
 # your bot username without @ like this ultron2_robot
 BOT_USERNAME = "ultron2_robot"
 # your owner id
