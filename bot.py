4269a47f34beec376013e3c5c36c02b2bd319a49
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2024-06-01 20:19:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,12 @@ import logging
 from vars import *
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatMemberUpdated, ChatJoinRequest
+import asyncio
+from pyrogram import filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
+from rishabh.users_db import get_served_users
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -134,6 +140,53 @@ async def autoapprove(client: thanos, message: ChatJoinRequest):
     except Exception as e:
         logger.error(f"Error in autoapprove handler: {e}")
 
+
+
+@thanos.on_message(filters.command("stats") & filters.user(SUDO_USERS))
+async def stats(client: thanos, message: Message):
+    users = len(await get_served_users())
+    await message.reply_text(
+        f"<u><b>ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {client.me.mention} :</b></u>\n\n➻ <b>ᴜsᴇʀs :</b> {users}\n"
+    )
+
+
+@thanos.on_message(filters.command("broadcast") & filters.user(SUDO_USERS))
+async def broadcast(cli: thanos, message: Message):
+    if message.reply_to_message:
+        x = message.reply_to_message.id
+        y = message.chat.id
+    else:
+        if len(message.command) < 2:
+            return await message.reply_text(
+                "<b>ᴇxᴀᴍᴘʟᴇ </b>:\n/broadcast [ᴍᴇssᴀɢᴇ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ]"
+            )
+        query = message.text.split(None, 1)[1]
+    susr = 0
+    served_users = []
+    susers = await get_served_users()
+    for user in susers:
+        served_users.append(int(user["user_id"]))
+    for i in served_users:
+        try:
+            m = (
+                await cli.copy_message(chat_id=i, from_chat_id=y, message_id=x)
+                if message.reply_to_message
+                else await cli.send_message(i, text=query)
+            )
+            susr += 1
+            await asyncio.sleep(0.2)
+        except FloodWait as e:
+            flood_time = int(e.value)
+            if flood_time > 200:
+                continue
+            await asyncio.sleep(flood_time)
+        except:
+            continue
+
+    try:
+        await message.reply_text(f"<b>ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴍᴇssᴀɢᴇ ᴛᴏ {susr} ᴜsᴇʀs.</b>")
+    except:
+        pass
 
 if __name__ == "__main__":
     thanos.run()
