2e51478bc5c0ef3f141133fe2920504f147c6a39
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2024-06-02 06:51:12 +0530

    Update users_db.py

--- rishabh/users_db.py
+++ rishabh/users_db.py
@@ -1,4 +1,17 @@
-from . import db
+from sys import exit
+
+from async_mongo import AsyncClient
+
+from bot import logger
+from vars import DB_URI
+
+try:
+    mongo = AsyncClient(DB_URI)
+    db = mongo["Assistant"]
+    logger.info("Connected to your Mongo Database.")
+except Exception as e:
+    logger.error(f"Failed to connect to your Mongo Database.\ {e}")
+    exit(1)
 
 usersdb = db["users"]
 
