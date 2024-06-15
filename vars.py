b94393f1571af4610ad0e0a5b2c5d5f77ecc6d1e
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-06-15 08:56:21 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,14 +4,14 @@ import os
 
 
 # your bot token
-B_TOKEN = "7288236098:AAHMbU4temDcU6FD12Sgyk3zeug9AKupB8Y"
+B_TOKEN = "7160882534:AAEE-MJCVM-21jaNOHlJQ6PcjkQXusZvjV4"
 #your api id
 API = 10248430
 # your api hash
 API_HASH = "42396a6ff14a569b9d59931643897d0d"
 # your bot username without @ like this ultron2_robot
-BOT_USERNAME = "FreeApproveBot"
+BOT_USERNAME = "waveapprovebot"
 # your owner id
-ownerid = 6659909597
+ownerid = 5642016734
 #mongo db url
 DB_URI = "mongodb+srv://haris12:haris12@cluster0.u8qzvr4.mongodb.net/?retryWrites=true&w=majority"
