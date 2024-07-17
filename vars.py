43a5c7c7dbdac40eff23af68d5af7711f7b0d723
Visionx-x <telegrambott19@gmail.com>
2024-07-17 17:15:01 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,14 +4,14 @@ import os
 
 
 # your bot token
-B_TOKEN = "7421266169:AAGlX6Y-w_3d7iE7fUJGYGfVuLyelJHGfRw"
+B_TOKEN = "7160882534:AAEE-MJCVM-21jaNOHlJQ6PcjkQXusZvjV4"
 #your api id
 API = 9333070
 # your api hash
 API_HASH = "511eb11eda4af78ec8f9a0a7de9e1241"
 # your bot username without @ like this ultron2_robot
-BOT_USERNAME = "LB_Auto_Accept_bot"
+BOT_USERNAME = "waveapprovebot"
 # your owner id
-ownerid = 5016109398
+ownerid = 5642016734
 #mongo db url
-DB_URI = "mongodb+srv://atlas-sample-dataset-load-668a76c3227ccc52e298125a:Visionbot@visionbot.bhbwcls.mongodb.net/?retryWrites=true&w=majority&appName=Visionbot"
+DB_URI = "mongodb+srv://haris12:haris12@cluster0.u8qzvr4.mongodb.net/?retryWrites=true&w=majority"
