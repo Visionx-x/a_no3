91b49830f941847a8016a6a517735deeafcd53b9
Visionx-x <telegrambott19@gmail.com>
2024-07-17 17:13:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,10 +131,26 @@ async def autoapprove(client: thanos, message: ChatJoinRequest):
 
         # Send a personal welcome message to the user
         personal_message = (
-            f"𝗛𝗲𝗹𝗹𝗼 👋 {message.from_user.mention},\n\n"
-"🤖 Want To Create Your own Telegram Bot ❓\n"
-"📍 Watch Videos from this Channel and create 📍\n\n"
-            "✨ Link :- https://youtube.com/@learning_bots?si=TDXxnpDkOPkEOM9U"
+            f"👋 𝗛𝗲𝗹𝗹𝗼 {message.from_user.mention}, Welcome to 𝗞𝗱𝗿𝗮𝗺𝗮𝗪𝗮𝘃𝗲\n\n"
+
+"🔰 𝗪𝗵𝗮𝘁 𝘆𝗼𝘂 𝘄𝗶𝗹𝗹 𝗴𝗲𝘁 𝗯𝘆 𝗝𝗼𝗶𝗻𝗶𝗻𝗴 𝗞𝗱𝗿𝗮𝗺𝗮𝗪𝗮𝘃𝗲?\n"
+"1⃣ All your favourite Korean & Chinese Drama in different audio like English Hindi Tamil\n"
+"2⃣ Drama with a Complete Season or Ongoing Episode\n"
+"3⃣ Watch Now and Download link of all the Dreams\n\n"
+
+"✊ 𝗕𝗲𝗰𝗼𝗺𝗲 𝗮 𝗺𝗲𝗺𝗯𝗲𝗿 𝗼𝗳 𝗼𝘂𝗿 𝗞𝗱𝗿𝗮𝗺𝗮𝗪𝗮𝘃𝗲 𝗖𝗼𝗺𝗺𝘂𝗻𝗶𝘁𝘆?\n"
+"1⃣ Request any Drama which you want to watch\n"
+"2⃣ If the Drama is available our Bot will provide you the link\n"
+"3⃣ Chat with Other Drama Lovers\n\n"
+
+"🔰 Drama online dekhe Hindi English Tamil etc languages me\n\n"
+
+"♥️ Our Community Joining Link 👇\n"
+"https://t.me/KDramaWave\n"
+"https://t.me/KDramaWave\n"
+"https://t.me/KDramaWave\n\n"
+
+"🔰 𝗦𝗲𝗻𝗱 /start 𝘁𝗼 𝗸𝗻𝗼𝘄 𝗺𝗼𝗿𝗲 𝗮𝗯𝗼𝘂𝘁 𝘁𝗵𝗶𝘀 𝗯𝗼𝘁."
         )
 
         await client.send_message(
