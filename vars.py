10ea5467c2e469dc606c8d3559f26e7dbba09635
Visionx-x <telegrambott19@gmail.com>
2024-09-18 12:26:58 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,14 +4,14 @@ import os
 
 
 # your bot token
-B_TOKEN = "7288236098:AAGr2Zvc7C_JyHUNg8xNjuaH5PSnzi7VDTM"
+B_TOKEN = "6999255941:AAGRdWuJlnIFI2E6sDm0dY_xFwCqwWKJUAY"
 #your api id
 API = 9333070
 # your api hash
 API_HASH = "511eb11eda4af78ec8f9a0a7de9e1241"
 # your bot username without @ like this ultron2_robot
-BOT_USERNAME = "FreeApproveBot"
+BOT_USERNAME = "AriseApproveBot"
 # your owner id
-ownerid = 6659909597
+ownerid = 6720082402
 #mongo db url
 DB_URI = "mongodb+srv://haris12:haris12@cluster0.u8qzvr4.mongodb.net/?retryWrites=true&w=majority"
